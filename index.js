fca7b79981dc6ba965f523fff3821ad651905ff9
Sabrina Jones <sabrina.jones87@gmail.com>
2017-12-27 00:25:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ let mealId = 0;
 let employerId = 0;
 let store = {drivers: [], deliveries:[], meals: [], employers: [], customers: []}
 
-class Customer {
-    constructor(name, employer) {
-        this.id = ++customerId;
-        if (name) {this.name = name};
-        if (employer) {this.employerId = employer.id};
-        store.customers.push(this);
-    }
+class Delivery {
+  constructor(meal={}, customer={}){
+    this.id = ++deliveryId
+    this.mealId = meal.id;
+    this.customerId = customer.id;
+    store.deliveries.push(this);
+  }
+  customer(){
+    return store.customers.find(customer=>{
+      return customer.id === this.customerId;
+    })
+  }
+  meal(){
+    return store.meals.find(meal=>{
+      return meal.id === this.mealId;
+    })
+  }
+}
